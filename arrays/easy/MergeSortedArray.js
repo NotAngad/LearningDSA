70156079b6be5b4bf3d74d9c43caec6ff1ed1862
Angad Srivastav <srivastav.angad@gmail.com>
2025-07-09 00:04:39 +0530

    feat: solved MergeSortedArray

---
+++ arrays/easy/MergeSortedArray.js
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ */
+const merge = (nums1 = [], m = 0, nums2 = [], n = 0) => {
+  for (let i = 0; i < n; i++) {
+    nums1[i + m] = nums2[i];
+  }
+  return nums1.sort((a, b) => a - b);
+};
+
+console.log(merge([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3));
