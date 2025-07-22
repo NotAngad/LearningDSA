c773098a38e4e8859e41b9f5dd058aa1c837a72f
Angad Srivastav <srivastav.angad@gmail.com>
2025-07-22 15:49:15 +0530

    feat: solved a problem

---
+++ strings/easy/RansomNote.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/ransom-note/description/
+ * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from
+ * magazine and false otherwise.Each letter in magazine can only be used once in ransomNote.
+ * Input: ransomNote = "a", magazine = "b"
+ * Output: false
+ */
+const canConstruct = (ransomNote, magazine) => {
+  const noteMap = {};
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    noteMap[ransomNote[i]] = (noteMap[ransomNote[i]] ?? 0) + 1;
+  }
+
+  for (let i = 0; i < magazine.length; i++) {
+    if (noteMap[magazine[i]]) {
+      noteMap[magazine[i]]--;
+      if (noteMap[magazine[i]] === 0) delete noteMap[magazine[i]];
+    }
+  }
+
+  return Object.keys(noteMap).length === 0;
+};
+
+console.log(canConstruct("aa", "ab"));
