fe71b1efd333999257483e3748c9bf321f8f4ea5
Angad Srivastav <srivastav.angad@gmail.com>
2025-07-23 17:42:00 +0530

    modified a problem

--- arrays/easy/DifferenceOfTwoArray.js
+++ arrays/easy/DifferenceOfTwoArray.js
@@ -1,5 +1,10 @@
 /**
- *
+ * https://leetcode.com/problems/find-the-difference-of-two-arrays/description/
+ * Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+ * answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+ * answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+ * Input: nums1 = [1,2,3], nums2 = [2,4,6]
+ * Output: [[1,3],[4,6]]
  * @param {number[]} nums1
  * @param {number[]} nums2
  */
