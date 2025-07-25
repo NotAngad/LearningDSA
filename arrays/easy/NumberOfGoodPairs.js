21c43a0abc8413911ac1beccfcd13364cf7aad58
Angad Srivastav <srivastav.angad@gmail.com>
2025-07-25 23:59:22 +0530

    feat: modified description

--- arrays/easy/NumberOfGoodPairs.js
+++ arrays/easy/NumberOfGoodPairs.js
@@ -1,6 +1,10 @@
 /**
- *
- * @param {number[]} nums
+ * https://leetcode.com/problems/number-of-good-pairs/
+ * Given an array of integers nums, return the number of good pairs.
+ * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+ * Input: nums = [1,2,3,1,1,3]
+ * Output: 4
+ * Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
  */
 var numIdenticalPairs = function (nums) {
   const map = {};
