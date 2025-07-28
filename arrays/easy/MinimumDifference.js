aa4a4f6c97f21df81e25fdf725b5eec7c1b751da
Angad Srivastav <srivastav.angad@gmail.com>
2025-07-29 01:35:21 +0530

    feat: two pointer problem solved

---
+++ arrays/easy/MinimumDifference.js
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+ * You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given
+ * an integer k. Pick the scores of any k students from the array so that the difference between the highest and the
+ * lowest of the k scores is minimized. Return the minimum possible difference.
+ * Input: nums = [9,4,1,7], k = 2
+ * Output: 2
+ * Explanation: There are six ways to pick score(s) of two students:
+ * [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
+ * [9,4,1,7]. The difference between the highest and lowest score is 9 - 1 = 8.
+ * [9,4,1,7]. The difference between the highest and lowest score is 9 - 7 = 2.
+ * [9,4,1,7]. The difference between the highest and lowest score is 4 - 1 = 3.
+ * [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
+ * [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
+ * The minimum possible difference is 2.
+ */
+const minimumDifference = (nums, k) => {
+  if (nums.length <= 1) return 0;
+  let difference = Infinity;
+
+  nums.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = k - 1;
+
+  while (right < nums.length) {
+    difference = Math.min(difference, nums[right] - nums[left]);
+    left++;
+    right++;
+  }
+
+  return difference;
+};
+
+console.log(
+  minimumDifference([87063, 61094, 44530, 21297, 95857, 93551, 9918], 6)
+);
