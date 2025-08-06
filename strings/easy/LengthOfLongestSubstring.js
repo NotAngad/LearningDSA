c6cd6cb251dcecfccdc80018b9f841986c55a016
Angad Srivastav <srivastav.angad@gmail.com>
2025-08-06 16:19:14 +0530

    feat: desc added

--- strings/easy/LengthOfLongestSubstring.js
+++ strings/easy/LengthOfLongestSubstring.js
@@ -1,3 +1,10 @@
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ * Given a string s, find the length of the longest substring without duplicate characters.
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ */
 const lengthOfLongestSubstring = (s = "") => {
   let max = 0;
   let low = 0;
