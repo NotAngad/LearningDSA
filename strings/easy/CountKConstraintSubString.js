7684a617122fb7497cdf926059675bee6c24ddaa
Angad Srivastav <srivastav.angad@gmail.com>
2025-08-06 16:14:38 +0530

    feat: desc added

--- strings/easy/CountKConstraintSubString.js
+++ strings/easy/CountKConstraintSubString.js
@@ -1,3 +1,11 @@
+/**
+ * https://leetcode.com/problems/count-substrings-that-satisfy-k-constraint-i/
+ * You are given a binary string s and an integer k. A binary string satisfies the k-constraint if either of the following
+ * conditions holds: The number of 0's in the string is at most k. The number of 1's in the string is at most k. Return an
+ * integer denoting the number of substrings of s that satisfy the k-constraint.
+ * Input: s = "10101", k = 1
+ * Output: 12
+ */
 const countKConstraintSubstrings = (s = "", k = 1) => {
   let map = {};
   let subString = 0;
